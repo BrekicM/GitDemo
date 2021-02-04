6efbab3ea42557fca775f107812679ad4ee198e8
BrekicM <brekic.marko@gmail.com>
2021-02-04 20:40:06 +0100

    Kreiranje projekta

---
+++ src/tests/DemoTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void demoTest() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+}
